temperature softmax fixed

model/utils/generation_utils.py
@@ -6,8 +6,13 @@ def temperature_scaled_softmax(logits, temperature=1.0, dim=0):
     if temperature > 0:
         res = torch.softmax(logits / temperature, dim=dim)
     else:
-        max_logits = torch.max(logits, dim=dim, keepdim=True)[0]
-        res = logits.ge(max_logits).to(dtype=logits.dtype)
+        chosen_token = torch.argmax(logits, dim=dim, keepdim=True)
+        res = torch.zeros_like(logits, device=logits.device, dtype=logits.dtype)
+        res.scatter_(dim=dim, index=chosen_token, src=torch.ones_like(chosen_token, device=logits.device, dtype=res.dtype))
+
+        # old implementation / dont work with equal max logits -> breaks greedy
+        # max_logits = torch.max(logits, dim=dim, keepdim=True)[0]
+        # res = logits.ge(max_logits).to(dtype=logits.dtype)
     return res
 
 