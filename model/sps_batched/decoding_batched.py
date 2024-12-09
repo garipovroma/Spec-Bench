f8b2f2c524f814722c33ee59294e1c8dc29fb69f
Dionysour <ismagilov.dv@phystech.edu>
2024-12-09 10:10:45 +0000

    use all forvard logits + fix of verified after eos

--- model/sps_batched/decoding_batched.py
+++ model/sps_batched/decoding_batched.py
@@ -27,6 +27,7 @@ def sps_batched_generate(
     bs = input_ids.shape[0]
     stats = BatchStats(batch_size=bs)
     stats.set_timer()
+    is_finished = torch.zeros(bs, dtype=torch.bool)
 
     for i in range(max_steps):
         generated_step = sps_batched_one_forward(
@@ -38,13 +39,15 @@ def sps_batched_generate(
             drafter,
             do_sample,
             temperature,
+            is_finished,
         )
 
         input_ids = generated_step["sequences"]
         attention_mask = generated_step["attention_mask"]
+        is_finished = generated_step["is_finished"]
         stats.add_accept(generated_step["verified"].reshape(-1, 1))
-    
-        if ((input_ids == model.generation_config.eos_token_id).any(-1).sum() == bs):
+
+        if (is_finished.sum() == bs):
             break
         
         if (stats.get_new_tokens() >= max_new_tokens).any():
@@ -64,53 +67,37 @@ def sps_batched_one_forward(
     drafter,
     do_sample=False,
     temperature=1,
+    is_finished=None,
 ):
     bs = input_ids.shape[0]
+    if is_finished is None:
+        is_finished = torch.zeros(bs, dtype=torch.bool)
+    max_matches = drafted_tokens + 1
 
     drafted = generate_with_pad(input_ids, attention_mask, drafter, do_sample, temperature, drafted_tokens, stop_on_eos=False)
-    
-    candidate_input_ids = drafted["sequences"]
-    candidate_logits = drafted["scores"][:, -drafted_tokens:]
+    candidate_input_ids = torch.cat([drafted["sequences"], torch.zeros(bs, 1, dtype=torch.long, device=drafted["sequences"].device)], 1)
+    candidate_logits = torch.cat([drafted["scores"][:, -drafted_tokens:], torch.zeros(bs, 1, drafted["scores"].shape[2], device=drafted["scores"].device)], 1)
     verifier_attention_mask=torch.cat([attention_mask, torch.ones((bs, drafted_tokens), device=attention_mask.device)], 1)
     
-    verifier_logits = model.forward(candidate_input_ids, attention_mask=verifier_attention_mask, position_ids=make_padded_pos_ids(verifier_attention_mask))["logits"]
+    verifier_logits = model.forward(drafted["sequences"], attention_mask=verifier_attention_mask, position_ids=make_padded_pos_ids(verifier_attention_mask))["logits"]
     
-    max_matches = drafted_tokens - 1
-    new_logits = verifier_logits[:, -drafted_tokens-1:-1]
+    new_logits = verifier_logits[:, -max_matches:]
 
     valid_tokens, n_matches = _speculative_sampling(candidate_input_ids, candidate_logits, drafted_tokens, new_logits, max_matches, do_sample, temperature)
-
-    # if do_sample:
-    #     verifier_logits = verifier_logits / temperature
-    #     new_logits = verifier_logits[:, -draft_tokens-1:-1]
-        
-    #     candidate_length = draft_tokens
-
-    #     valid_tokens, n_matches = _speculative_sampling(candidate_input_ids, candidate_logits, draft_tokens, new_logits, False, max_matches)
-    # else:
-    #     new_verifier_tokens = verifier_logits[:, -draft_tokens-1:-1].argmax(-1)
-    #     new_drafter_tokens = drafted["sequences"][:, -draft_tokens:]
-        
-    #     is_accepted = (new_verifier_tokens == new_drafter_tokens)
-    #     n_matches = ((~is_accepted).cumsum(dim=-1) < 1).sum(-1)
-    #     n_matches = torch.minimum(n_matches, torch.ones(bs, device=n_matches.device, dtype=torch.long) * max_matches)
-
-    #     new_token = torch.take_along_dim(new_verifier_tokens, n_matches.reshape(bs, 1), dim=1)
-
-    #     if (n_matches > 0).any():
-    #         valid_tokens = torch.cat((new_drafter_tokens[:, :n_matches.max()], new_token), dim=-1)
-    #     else:
-    #         valid_tokens = new_token
-
-    #     valid_tokens = valid_tokens
         
     new_attention_mask = (torch.arange(n_matches.max().item(), device=n_matches.device).repeat((bs,1)) < n_matches.reshape(bs, 1)).to(dtype=torch.long)
     new_attention_mask = torch.cat([new_attention_mask, torch.ones(bs, 1, device=new_attention_mask.device, dtype=torch.long)], -1)
+
     valid_tokens = torch.where(new_attention_mask == 1, valid_tokens, model.generation_config.pad_token_id)
- 
+
+    verified = (n_matches + 1) * ~is_finished.to(n_matches.device)
+
+    is_finished = (is_finished.to(valid_tokens.device) | (valid_tokens == model.generation_config.eos_token_id).any(-1))
+
     return {"sequences" : torch.cat([input_ids, valid_tokens], -1),
             "attention_mask" : torch.cat([attention_mask, new_attention_mask], -1), 
-            "verified" : n_matches + 1}
+            "verified" : verified,
+            "is_finished" : is_finished}
 
 
 @torch.no_grad()
@@ -157,16 +144,14 @@ def _speculative_sampling(
     batch_size = candidate_input_ids.shape[0]
     device = candidate_input_ids.device
     
-    new_candidate_input_ids = candidate_input_ids[:, -candidate_length:]
+    new_candidate_input_ids = candidate_input_ids[:, -max_matches:]
 
     q = temperature_scaled_softmax(candidate_logits, temperature, dim=-1)
     q_i = torch.gather(q, 2, new_candidate_input_ids[:, :, None]).squeeze(-1)
 
     p = temperature_scaled_softmax(new_logits, temperature, dim=-1)
     p_i = torch.gather(p, 2, new_candidate_input_ids[:, :, None]).squeeze(-1)
-
     probability_ratio = torch.clamp(p_i / q_i, min=0, max=1)
-
     r_i = torch.rand_like(probability_ratio)
     is_accepted = r_i <= probability_ratio
 
@@ -175,10 +160,10 @@ def _speculative_sampling(
     
     p_n_plus_1 = torch.take_along_dim(p, n_matches.resize(batch_size, 1, 1), 1)
     q_n_plus_1 = torch.take_along_dim(q, n_matches.resize(batch_size, 1, 1), 1)
-    p_prime = torch.clamp((p_n_plus_1 - q_n_plus_1 * (n_matches < max_matches).reshape(batch_size, 1, 1)), min=0).squeeze(1)
+    p_prime = torch.clamp((p_n_plus_1 - q_n_plus_1 * (n_matches < max_matches-1).reshape(batch_size, 1, 1)), min=0).squeeze(1)
 
     new_token = torch.multinomial(p_prime, num_samples=1)
-
+ 
     if (n_matches > 0).any():
         valid_tokens = torch.cat((new_candidate_input_ids[:, :n_matches.max()], new_token), dim=-1)
     else:
